0202e13c760dbc3f4530ccc7b538563b26b78587
tilianumadalin <53651906+tilianumadalin@users.noreply.github.com>
2023-01-08 11:23:37 +0200

    Update Auction.py

--- Auction.py
+++ Auction.py
@@ -1,26 +1,26 @@
 #LICITATIE
-# persons_bids = [{"x":0}]
-# user_answer = 'yes'
-# bids_amount_list = []
-#
-# def add_bids(money):
-#     bids_amount_list.append(bid)
-#
-# def add_persons(name,bid_amount):
-#     persons_bids.append({person:bid})
-#
-# while user_answer =='yes':
-#     person = input("What is your name?\n")
-#     bid = int(input("What is your bid?\n"))
-#     add_persons(person,bid)
-#     ask_user = input("Anyone else wants to bid? type yes or no\n").lower()
-#     user_answer = ask_user
-#     add_bids(bid)
-#     for n in bids_amount_list:
-#         max = 0
-#         if n>max:
-#             max=n
-#         else:
-#             max=max
-#
-# print(f"The winner is who paid {max} ")
+persons_bids = [{"x":0}]
+user_answer = 'yes'
+bids_amount_list = []
+
+def add_bids(money):
+    bids_amount_list.append(bid)
+
+def add_persons(name,bid_amount):
+    persons_bids.append({person:bid})
+
+while user_answer =='yes':
+    person = input("What is your name?\n")
+    bid = int(input("What is your bid?\n"))
+    add_persons(person,bid)
+    ask_user = input("Anyone else wants to bid? type yes or no\n").lower()
+    user_answer = ask_user
+    add_bids(bid)
+    for n in bids_amount_list:
+        max = 0
+        if n>max:
+            max=n
+        else:
+            max=max
+
+print(f"The winner is who paid {max} ")
